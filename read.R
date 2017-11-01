63e50e960b30e6ba8fdc0ed1510c1dbfde0d4612
WillForan <willforan@gmail.com>
2017-11-01 14:05:24 -0400

    graph and simple stats

--- read.R
+++ read.R
@@ -61,11 +61,29 @@ read.forATT <- function(f) {
   d$MatterID      <- allinfo$`Matter ID`
   d$AccountNumber <- allinfo$`Account Number`
   d$FromNumber    <- mknum(allinfo$`Voice Usage For`)
+
+  # baseless assumption:
+  #  forward and dialed mean mostly the same thing 
+  #  or at least neither exist at the same time
+  unexpectedidx <- which( !is.na(d$FORWARDED) & !is.na(d$DIALED) )
+  if(length(unexpectedidx) > 0L  ) warning('ATT from: bad assumption about DIAL and FORWARDED never conflicting! For indexes:',unexpectedidx)
+
+  # make common ConnectedTo column from FORWARDED and DIALED
+  noForward <- is.na(attfor1$FORWARDED)
+  d$ConnectedTo <- attfor1$FORWARDED
+  d$ConnectedTo[noForward] <- attfor1$DIALED[noForward]
+
   return(d)
 }
 
 read.forTmoble <-function(f) {
-  d <- read_xlsx(f,skip=2)
+  # use dplyr "pipeline" style to 
+  # rename and "mutate" (change) connected to
+  d <-
+    read_xlsx(f,skip=2) %>%
+    rename(ConnectedTo ='Connected To') %>% 
+    mutate(ConnectedTo = mknum(ConnectedTo)) # make phone number a number
+
   return(d)
 }
 
@@ -76,11 +94,67 @@ read.CDR_Tmoble<-function(f) {
 
 ###### 
 
-# get file, Sys.glob  adn "*" instad of writting out the phone number
+# get file, Sys.glob to use  "*" instad of writting out the phone number
 file.att.for.2 <- Sys.glob('data/ATT raw calls data for * 2nd file.txt')
 attfor1 <- read.forATT(file.att.for.2)
 
 file.tmobile.for <- Sys.glob('data/TMobile raw calls data for *.xlsx')
 tmobilefor <- read.forTmoble(file.tmobile.for)
 
+## some stats
 
+# what numbers do these phones call in common
+calledsame <- which(tmobilefor$ConnectedTo %in% unique(na.omit(attfor1$ConnectedTo)))
+# none
+
+## summary stats
+tmbl.withcnt <- 
+    # from tmobile data
+    tmobilefor %>%  
+    # trim columns and rename them to easier no space version
+    select(Event=`Event Type`,ConnectedTo,Direction,Dur=`Duration(Seconds)`)  %>%
+    # remove where there is no direction (only keep when not NA)
+    filter(!is.na(Direction)) %>% 
+    # run stats within each number (each number will have it's own count in column n)
+    group_by(ConnectedTo) %>%  
+    # generate counts (but don't collapse data -- mutate instead of summarise)
+    # so still one row per contact instance but each number has it's n repeated for each instance
+    mutate(n=n())  
+
+## visualzie
+# plot the summary, but only for call+sms
+tmbl.withcnt %>% 
+  filter(nchar(ConnectedTo)==11) %>% 
+  # send the filter data to plot
+  # noteice we use + instead of %>% to 'add' plot features (instead of pipeing)
+  ggplot() + 
+  # aesthetics: x is event, so is our color fill
+  aes(x=Event,fill=Event) +
+  # generate a histogram
+  geom_histogram(stat='count') +
+  # default theme is hard sometimes, so use black and white one
+  theme_bw() +  
+  ggtitle('frequence of event type for numbers contacted more than 11 times')
+
+# actually summarize. 
+# now we only have one row for each number + direction combo
+tmbl.smry <- 
+    tmbl.withcnt %>%
+    group_by(Direction,ConnectedTo) %>%
+    summarise( sumdur= sum(Dur), n=n() )
+
+ggplot(tmbl.smry) + 
+    # set x, y, and other aesthetics
+    aes(x=Direction,y=sumdur,color=n,label=ConnectedTo) + 
+    # and points to the graph
+    geom_point()  +
+    # use a line to connect numbers (group by the phone number)
+    geom_line(aes(group=ConnectedTo)) +
+    # label numbers with more than 15 contact events
+    geom_label(data=tmbl.smry %>% filter(n>15),
+               size=3, alpha=.3,
+               vjust=-1,hjust=-.1) +
+    theme_bw() +
+    ggtitle('incoming vs outgoing duration + number of contacts (inc SMS)')
+# save it
+ggsave('inVsOut.png')
